21f0142303c221fae8c209b3850233c76a627133
Alessandro Sangiorgi <mail@alessandrosangiorgi.net>
2017-12-09 10:14:38 +0100

    Bounded

--- more_brightness.sh
+++ more_brightness.sh
@@ -2,4 +2,9 @@
 
 currentbrightness=$(xrandr --current --verbose | grep eDP1 -A 5 | grep Brightness |  cut -d ":" -f 2)
 newerbrightness=$(echo "$currentbrightness + 0.1" | bc )
-xrandr --output eDP1 --brightness $newerbrightness
+
+comparison=$(echo "$newerbrightness < 1.1" | bc -l)
+if [ $comparison -eq "1" ]
+	then
+	xrandr --output eDP1 --brightness $newerbrightness
+fi
